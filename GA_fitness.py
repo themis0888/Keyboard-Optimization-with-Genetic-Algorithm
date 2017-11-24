8939ef5dc4099c7424dd236c62e31aa9730ec6cd
bj <bjk94@naver.com>
2017-11-24 18:58:34 +0900

    added naive area-fitness function

--- GA_fitness.py
+++ GA_fitness.py
@@ -1,6 +1,32 @@
+from solution import Solution
+from config import CONFIG
+
 # fitness function
 def fitness(sol):
-    return 0
+    fit_val = 0
+    
+    fit_val += 1.0 * fitness_area(sol)
+
+    return fit_val
 
 # auxiliary fitness functions
-# ...
+
+# area
+def fitness_area(sol):
+    avg_size = CONFIG['keyboard_width'] * CONFIG['keyboard_height'] / Solution.num_alphabet
+    
+    fit_val = 0
+
+    for i in range(Solution.num_alphabet):
+        A = sol.areas[i]
+        Ar = sol.areas_rect[i]
+
+        if A >= avg_size:
+            if A / Ar >= 0.5:
+                pass
+            else:
+                fit_val += (Ar - A) / Ar
+        else:
+            fit_val += (avg_size - A) / avg_size
+
+    return fit_val
